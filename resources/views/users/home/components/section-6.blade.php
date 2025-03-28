10bb005d75aac2dbf62d1c4db802bb18d4e55d13
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-03-28 14:22:01 +0700

    update

--- resources/views/users/home/components/section-6.blade.php
+++ resources/views/users/home/components/section-6.blade.php
@@ -169,11 +169,12 @@
             /* เพิ่มเงา */
         }
 
-        .bg-footer-comment{
+        .bg-footer-comment {
             background-color: #42b7e8;
             border-bottom-left-radius: 20px;
             border-bottom-right-radius: 20px;
         }
+
     </style>
 </head>
 
@@ -184,53 +185,44 @@
                 <div class="title-section6">
                     ข่าวประชาสัมพันธ์
                 </div>
-                <img src="{{ asset('pages/home/section-6/กรอบข่าวประชาสัมพันธ์.png') }}" alt="topper"
-                    class="mt-4 img-fluid px-2" style="z-index: 2; margin-bottom:-7px;">
+                <img src="{{ asset('pages/home/section-6/กรอบข่าวประชาสัมพันธ์.png') }}" alt="topper" class="mt-4 img-fluid px-2" style="z-index: 2; margin-bottom:-7px;">
                 <div class="bg-blue-section6 d-flex flex-column justify-content-center px-3 py-4">
-                    @for ($i = 0; $i < 3; $i++)
-                        <a href="#"
-                            class="bg-card-section6 pt-2 px-3 pb-4 d-flex flex-column justify-content-center align-items-center  mb-2">
-                            <div class="d-flex flex-column flex-md-row justify-content-center align-items-start gap-2">
-                                <img src="{{ asset('pages/home/section-6/ปกข่าวประชาสัมพันธ์.png') }}" alt="logo"
-                                    class="bg-white">
-                                <div class="text d-flex flex-column justify-content-center">
-                                    <div
-                                        class="text-dark bg-white py-1 py-md-3 py-lg-1 py-xl-3 px-3 lh-sm fs-6 rounded-4">
-                                        <?php
+                    @for ($i = 0; $i < 3; $i++) <a href="#" class="bg-card-section6 pt-2 px-3 pb-4 d-flex flex-column justify-content-center align-items-center  mb-2">
+                        <div class="d-flex flex-column flex-md-row justify-content-center align-items-start gap-2">
+                            <img src="{{ asset('pages/home/section-6/ปกข่าวประชาสัมพันธ์.png') }}" alt="logo" class="bg-white">
+                            <div class="text d-flex flex-column justify-content-center">
+                                <div class="text-dark bg-white py-1 py-md-3 py-lg-1 py-xl-3 px-3 lh-sm fs-6 rounded-4">
+                                    <?php
                                         $text = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ullam asperiores molestiae aliquid sunt atque eaque rem, totam nesciunt praesentium tenetur ut quibusdam quam perspiciatis ad numquam hic repellendus at veniam!
                                                                                                                                                                                                                                             ';
                                         $maxLength = 140; // กำหนดจำนวนตัวอักษรที่ต้องการให้แสดง
                                         echo mb_substr($text, 0, $maxLength) . (mb_strlen($text) > $maxLength ? '...' : '');
                                         ?>
-                                    </div>
                                 </div>
                             </div>
-                            <div class="w-100 mt-4 rounded-4 ms-3 me-2" style="border:2px solid #ffff;">
-                                {{-- เส็น --}}
-                            </div>
-                            <div class="bg-white text-dark fw-bold d-flex justify-content-center align-items-center py-1 px-2 rounded-4 text-decoration control-date"
-                                style="position: absolute; font-size:14px;">
-                                <img src="{{ asset('pages/home/section-6/นาฬิกา.png') }}" alt="clock"
-                                    style="width: 15px; height:15px;" class="me-1">
-                                dd/mm/yyyy
-                            </div>
+                        </div>
+                        <div class="w-100 mt-4 rounded-4 ms-3 me-2" style="border:2px solid #ffff;">
+                            {{-- เส็น --}}
+                        </div>
+                        <div class="bg-white text-dark fw-bold d-flex justify-content-center align-items-center py-1 px-2 rounded-4 text-decoration control-date" style="position: absolute; font-size:14px;">
+                            <img src="{{ asset('pages/home/section-6/นาฬิกา.png') }}" alt="clock" style="width: 15px; height:15px;" class="me-1">
+                            dd/mm/yyyy
+                        </div>
 
                         </a>
-                    @endfor
-                    <a href="#" class="btn-viewall-section6 px-4 py-1 mt-1 ms-auto">
-                        <img src="{{ asset('pages/home/section-5/door-icon.png') }}" alt="icon-door"> ดูทั้งหมด</a>
+                        @endfor
+                        <a href="#" class="btn-viewall-section6 px-4 py-1 mt-1 ms-auto">
+                            <img src="{{ asset('pages/home/section-5/door-icon.png') }}" alt="icon-door"> ดูทั้งหมด</a>
                 </div>
             </div>
             <div class="col-xl-6 d-flex flex-column justify-content-center align-items-center">
                 <div class="d-flex flex-column flex-lg-row justify-content-center align-items-center">
                     <div class="d-flex flex-column justify-content-center align-items-center mt-4">
                         <a href="#" class="banner-hover px-3 py-1">
-                            <img src="{{ asset('pages/home/section-6/BN เฟสบุ๊ค.png') }}" alt="facebook"
-                                class="img-fluid ">
+                            <img src="{{ asset('pages/home/section-6/BN เฟสบุ๊ค.png') }}" alt="facebook" class="img-fluid ">
                         </a>
                         <a href="#" class="banner-hover px-3 py-1">
-                            <img src="{{ asset('pages/home/section-6/BN ห้องสมุด.png') }}" alt="e-library"
-                                class="img-fluid">
+                            <img src="{{ asset('pages/home/section-6/BN ห้องสมุด.png') }}" alt="e-library" class="img-fluid">
                         </a>
                     </div>
                     <a href="#" class="banner-hover px-3 pt-2 pb-4">
@@ -243,74 +235,70 @@
             </div>
         </div>
         <div class="col-12 d-flex flex-column mt-5">
-            <div class="bg-white d-flex flex-column justify-content-center align-items-start p-2"
-                style="border-top-left-radius: 10px; border-top-right-radius: 10px;">
+            <div class="bg-white d-flex flex-column justify-content-center align-items-start p-2" style="border-top-left-radius: 10px; border-top-right-radius: 10px;">
                 <div class="d-flex justify-content-between w-100">
-                    <div class="title-coomment py-1 ps-1 pe-3 d-flex justify-content-center align-items-center"
-                    style="margin-top: -1rem; margin-left:-7px;">
-                    <img src="{{ asset('pages/home/section-6/icon-comment.png') }}" alt="icon" class="img-fluid"
-                        style="margin-top: -1rem;">
-                    <div class="text-title lh-1 ms-2">
-                        <div class="main-title">
-                            แสดงความคิดเห็น
-                        </div>
-                        <div class="sub-title">
-                            อยากให้อบต.บางพลวงเร่งแก้ปัญหาเรื่องใด
+                    <div class="title-coomment py-1 ps-1 pe-3 d-flex justify-content-center align-items-center" style="margin-top: -1rem; margin-left:-7px;">
+                        <img src="{{ asset('pages/home/section-6/icon-comment.png') }}" alt="icon" class="img-fluid" style="margin-top: -1rem;">
+                        <div class="text-title lh-1 ms-2">
+                            <div class="main-title">
+                                แสดงความคิดเห็น
+                            </div>
+                            <div class="sub-title">
+                                อยากให้อบต.บางพลวงเร่งแก้ปัญหาเรื่องใด
+                            </div>
                         </div>
                     </div>
-                </div>
-                <div class="lay-out-btn-accept px-1 py-3 ms-auto me-4 d-none d-md-block">
-                    <a href="#" class="btn-accept-comment p-2">กดโหวต</a>
-                </div>
+                    <form action="" method="POST">
+                        <div class="lay-out-btn-accept px-2 py-2 ms-auto me-4 d-none d-md-block">
+                            {{-- <a href="#" class="btn-accept-comment p-2">กดโหวต</a> --}}
+                            <button type="submit" class="btn-accept-comment p-2">กดโหวต</button>
+                        </div>
                 </div>
                 <div class="d-flex flex-column justify-content-start align-items-center">
                     <div class="row p-3">
                         <div class="col-md-6 col-lg-4">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="option" id="option1"
-                                    value="1">
+                                <input class="form-check-input" type="radio" name="option" id="option1" value="1">
                                 <label class="form-check-label" for="option1">จัดการเรื่องการป้องกันน้ำท่วม</label>
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-4">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="option" id="option2"
-                                    value="2">
+                                <input class="form-check-input" type="radio" name="option" id="option2" value="2">
                                 <label class="form-check-label" for="option2">แก้ไขปัญหาสิ่งแวดล้อม</label>
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-4">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="option" id="option3"
-                                    value="3">
+                                <input class="form-check-input" type="radio" name="option" id="option3" value="3">
                                 <label class="form-check-label" for="option3">แก้ไขปัญหาไฟฟ้าดับบ่อย</label>
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-4">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="option" id="option4"
-                                    value="4">
+                                <input class="form-check-input" type="radio" name="option" id="option4" value="4">
                                 <label class="form-check-label" for="option4">จัดมาตรการป้องกันน้ำท่วม</label>
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-4">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="option" id="option5"
-                                    value="5">
+                                <input class="form-check-input" type="radio" name="option" id="option5" value="5">
                                 <label class="form-check-label" for="option5">แก้ไขปัญหายาเสพติด</label>
                             </div>
                         </div>
                         <div class="col-md-6 col-lg-4">
                             <div class="form-check">
-                                <input class="form-check-input" type="radio" name="option" id="option6"
-                                    value="6">
+                                <input class="form-check-input" type="radio" name="option" id="option6" value="6">
                                 <label class="form-check-label" for="option6">แก้ไขปัญหาลักขโมย</label>
                             </div>
                         </div>
                     </div>
-                    <div class="lay-out-btn-accept px-1 py-3  me-4 d-block d-md-none">
-                        <a href="#" class="btn-accept-comment p-2">กดโหวต</a>
+
+
+                    <div class="lay-out-btn-accept px-2 py-2  me-4 d-block d-md-none">
+                        <button type="submit" class="btn-accept-comment p-2">กดโหวต</button>
                     </div>
+                </form>
                 </div>
             </div>
             <div class="bg-footer-comment p-3">
