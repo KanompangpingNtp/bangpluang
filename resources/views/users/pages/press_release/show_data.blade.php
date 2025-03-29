3116d258453b25933fe50a499f70d08d2a5e461b
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-03-29 10:07:03 +0700

    ีupdate

--- resources/views/users/pages/press_release/show_data.blade.php
+++ resources/views/users/pages/press_release/show_data.blade.php
@@ -96,7 +96,9 @@
 
                             <!-- รูปภาพด้านซ้าย -->
                             <div style="flex: 0 0 100px; height: 100px; overflow: hidden; border-radius: 10px;">
-                                <img src="{{ asset('storage/' . ($post->photos->first()->post_photo_file ?? 'pages/home/section-5/LOGOบางพลวง.png')) }}" alt="Image {{ $index + 1 }}" style="width: 100%; height: 100%; object-fit: cover;">
+                                <img src="{{ asset('storage/' . (isset($post->photos) && $post->photos->isNotEmpty() ? $post->photos->first()->post_photo_file : 'pages/home/section-5/LOGOบางพลวง.png')) }}"
+                                alt="Image {{ $index + 1 }}" style="width: 100%; height: 100%; object-fit: cover;">
+
                             </div>
 
                             <!-- ข้อความด้านขวา -->
