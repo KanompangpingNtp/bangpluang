e5988cce3b0e3e1dbd4b96926f51bb2593606e27
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-03-29 11:03:41 +0700

    update

--- app/Http/Controllers/procurement/ProcurementController.php
+++ app/Http/Controllers/procurement/ProcurementController.php
@@ -4,8 +4,71 @@ namespace App\Http\Controllers\procurement;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\AuthorityType;
+use App\Models\PostDetail;
+use App\Models\PersonnelAgency;
+use App\Models\OperationalPlanType;
+use App\Models\LawsRegsType;
+use App\Models\PublicMenusType;
+use App\Models\PerfResultsType;
 
 class ProcurementController extends Controller
 {
-    //
+    public function ProcurementShowData()
+    {
+        $personnelAgencies = PersonnelAgency::with('ranks')
+        ->whereIn('status', [1, 2, 3, 4, 5])
+        ->orderByRaw("FIELD(status, 1, 2, 3, 4, 5)")
+        ->get();
+
+        $PerfResultsMenu = PerfResultsType::all();
+        $AuthorityMenu = AuthorityType::all();
+        $OperationalPlanMenu = OperationalPlanType::all();
+        $LawsRegsMenu = LawsRegsType::all();
+        $PublicMenus = PublicMenusType::all();
+
+        $Procurement = PostDetail::with('postType','photos')
+            ->whereHas('postType', function ($query) {
+                $query->where('type_name', 'ประกาศจัดซื้อจัดจ้าง');
+            })->paginate(14);
+
+        return view('users.pages.procurement.show_data', compact(
+            'Procurement',
+            'PublicMenus',
+            'personnelAgencies',
+            'PerfResultsMenu',
+            'AuthorityMenu',
+            'OperationalPlanMenu',
+            'LawsRegsMenu'
+        ));
+    }
+
+    public function ProcurementDetail($id)
+    {
+        $personnelAgencies = PersonnelAgency::with('ranks')
+        ->whereIn('status', [1, 2, 3, 4, 5])
+        ->orderByRaw("FIELD(status, 1, 2, 3, 4, 5)")
+        ->get();
+
+        $PerfResultsMenu = PerfResultsType::all();
+        $AuthorityMenu = AuthorityType::all();
+        $OperationalPlanMenu = OperationalPlanType::all();
+        $LawsRegsMenu = LawsRegsType::all();
+        $PublicMenus = PublicMenusType::all();
+
+        $Procurement = PostDetail::with(['pdfs'])
+            ->whereHas('postType', function ($query) {
+                $query->where('type_name', 'ประกาศจัดซื้อจัดจ้าง');
+            })->findOrFail($id);
+
+        return view('users.pages.procurement.show_detail', compact(
+            'Procurement',
+            'PublicMenus',
+            'personnelAgencies',
+            'PerfResultsMenu',
+            'AuthorityMenu',
+            'OperationalPlanMenu',
+            'LawsRegsMenu'
+        ));
+    }
 }
--- app/Http/Controllers/procurement_results/ProcurementResultsController.php
+++ app/Http/Controllers/procurement_results/ProcurementResultsController.php
@@ -4,8 +4,72 @@ namespace App\Http\Controllers\procurement_results;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\AuthorityType;
+use App\Models\PostDetail;
+use App\Models\PersonnelAgency;
+use App\Models\OperationalPlanType;
+use App\Models\LawsRegsType;
+use App\Models\PublicMenusType;
+use App\Models\PerfResultsType;
 
 class ProcurementResultsController extends Controller
 {
-    //
+    public function ProcurementResultsDetail($id)
+    {
+        $personnelAgencies = PersonnelAgency::with('ranks')
+        ->whereIn('status', [1, 2, 3, 4, 5])
+        ->orderByRaw("FIELD(status, 1, 2, 3, 4, 5)")
+        ->get();
+
+        $PerfResultsMenu = PerfResultsType::all();
+        $AuthorityMenu = AuthorityType::all();
+        $OperationalPlanMenu = OperationalPlanType::all();
+        $LawsRegsMenu = LawsRegsType::all();
+        $PublicMenus = PublicMenusType::all();
+
+        $ProcurementResults = PostDetail::with(['pdfs'])
+            ->whereHas('postType', function ($query) {
+                $query->where('type_name', 'ผลประกาศจัดซื้อจัดจ้าง');
+            })->findOrFail($id);
+
+        return view('users.pages.procurementResults.show_detail', compact(
+            'ProcurementResults',
+            'personnelAgencies',
+            'PerfResultsMenu',
+            'AuthorityMenu',
+            'OperationalPlanMenu',
+            'LawsRegsMenu',
+            'PublicMenus'
+        ));
+    }
+
+
+    public function ProcurementResultsShowData()
+    {
+        $personnelAgencies = PersonnelAgency::with('ranks')
+        ->whereIn('status', [1, 2, 3, 4, 5])
+        ->orderByRaw("FIELD(status, 1, 2, 3, 4, 5)")
+        ->get();
+
+        $PerfResultsMenu = PerfResultsType::all();
+        $AuthorityMenu = AuthorityType::all();
+        $OperationalPlanMenu = OperationalPlanType::all();
+        $LawsRegsMenu = LawsRegsType::all();
+        $PublicMenus = PublicMenusType::all();
+
+        $ProcurementResults = PostDetail::with('postType','photos')
+            ->whereHas('postType', function ($query) {
+                $query->where('type_name', 'ผลประกาศจัดซื้อจัดจ้าง');
+            })->paginate(14);
+
+        return view('users.pages.procurementResults.show_data', compact(
+            'ProcurementResults',
+            'PublicMenus',
+            'personnelAgencies',
+            'PerfResultsMenu',
+            'AuthorityMenu',
+            'OperationalPlanMenu',
+            'LawsRegsMenu'
+        ));
+    }
 }
