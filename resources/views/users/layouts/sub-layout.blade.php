1dcdbd640761925a64d746ce638b5f7fad8ba366
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-04-02 09:42:09 +0700

    update

--- resources/views/users/layouts/sub-layout.blade.php
+++ resources/views/users/layouts/sub-layout.blade.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="คำอธิบายเว็บไซต์">
-    <title>@yield('title', 'Default Title')</title>
+    <title>@yield('title', 'องค์การบริหารส่วนตำบลบางพลวง')</title>
+    <link rel="icon" type="image/png" href="{{ asset('navbar/logo.png') }}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
     <!-- Swiper CSS -->
@@ -680,7 +681,7 @@
                                             class="navbar-icon">
 
                                     </a>
-                                    <ul class="dropdown-menu" 
+                                    <ul class="dropdown-menu"
                                         aria-labelledby="authorityDropdown">
                                         @foreach ($AuthorityMenu as $detail)
                                             <li>
